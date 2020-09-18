9583b5b003f8c7c6222756bd7c840baca7643296
Kaue <uriel.kau39@gmail.com>
2020-09-18 13:49:14 -0300

    Troca de rep

---
+++ Projeto4/Assets/Scripts/Movimentacao.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimentacao : MonoBehaviour
+{
+    public float velocidade;
+    public FloatingJoystick joystick;
+    private float h;
+    private float v;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movimentação no teclado: ADWS
+        //h = Input.GetAxis("Horizontal");
+        //v = Input.GetAxis("Vertical");
+
+        //Movimentação no joystick virtual
+        h = joystick.Horizontal;
+        v = joystick.Vertical;
+
+        transform.Translate(new Vector3(h, v, 0) * velocidade * Time.deltaTime);
+        
+    }
+}
