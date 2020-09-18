17b0c38c8ab505e74846353ab645615b83ba88e2
Kaue <uriel.kau39@gmail.com>
2020-09-18 19:38:17 -0300

    iads

---
+++ Projeto4/Assets/Scripts/PullingObjects/Pull.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pull : MonoBehaviour
+{
+    public Transform theDest;
+    
+    private void grab()
+    {
+        this.transform.position = theDest.transform.position;
+        this.transform.parent = GameObject.Find("Destination").transform;
+    }
+
+    private void disgrab()
+    {
+        this.transform.position = this.transform.position;;
+    }
+
+
+
+}
---
+++ Projeto4/Assets/Scripts/player/playerpush.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerpush : MonoBehaviour
+{
+	public float distance = 1f;
+	
+
+	
+    bool pulling =false;
+	GameObject box;
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, distance);
+
+		if (hit.collider !=null && hit.collider.tag=="pushable")
+		{
+			
+			if (Input.GetKeyDown(KeyCode.E))
+			{
+				hit.collider.gameObject.GetComponent<Pull>().enabled = true;
+
+
+				Debug.Log("Pegou");
+			}else if (Input.GetKeyUp(KeyCode.E))
+			{
+				hit.collider.gameObject.GetComponent<Pull>().enabled = false;
+
+			}
+
+			Debug.Log("Ta vendo");
+
+
+		}
+		
+
+	}
+
+
+	
+
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.yellow;
+
+		Gizmos.DrawLine(transform.position, (Vector2)transform.position + Vector2.right * transform.localScale.x * distance);
+
+
+
+	}
+}
