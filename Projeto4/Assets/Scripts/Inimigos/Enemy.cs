414a7b8d074a15f0753a47f74d22db43f4eb592e
pedroborges13 <pedroborges13@users.noreply.github.com>
2020-10-07 07:25:27 -0300

    inimigo v1
    v1

---
+++ Projeto4/Assets/Scripts/Inimigos/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public float distance;
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {     
+            transform.eulerAngles = new Vector3(0, 180, 0);
+            Debug.Log("deu certo");      
+    }
+}
