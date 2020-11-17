dc77ff9a371cce47ec883725a2448f53448bac5b
BeatrizUrgal <beaurgalmoreno@gmail.com>
2020-11-17 16:00:06 -0300

    Sistema de Vida Tela Game Over

--- Projeto4/Assets/Scripts/player/VidaJogador.cs
+++ Projeto4/Assets/Scripts/player/VidaJogador.cs
@@ -1,17 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VidaJogador : MonoBehaviour
 {
+    MonetizationManager monetizationManager;
 
     public int vidaMaxima;
     public int vidaAtual;
+    public Text texto;
+    private GameOver GameOverScript;
 
-    // Start is called before the first frame update
+    
+    void Awake()
+    {
+
+        GameOverScript = GetComponent<GameOver>();
+    }
+
     void Start()
     {
+        monetizationManager = FindObjectOfType<MonetizationManager>();
         vidaAtual = vidaMaxima;
+        texto = GameObject.Find("Player_Life").GetComponent<Text>();
+        texto.text = vidaAtual.ToString();
     }
 
     // Update is called once per frame
@@ -23,10 +36,16 @@ public class VidaJogador : MonoBehaviour
     public void ReceberDano()
     {
         vidaAtual -= 1;
+        texto.text = vidaAtual.ToString();
 
         if (vidaAtual <= 0)
         {
             Debug.Log("Game Over");
+            //GameOver Component.GetComponent<GameOver>().openMorteMenu();
+        }
+        else
+        {
+            monetizationManager.ShowInterstitial();
         }
     }
 }
