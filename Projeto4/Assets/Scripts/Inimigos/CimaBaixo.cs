f55f0536a705ccd1aea4a457ea24d233d6308925
pedroborges13 <pedroborges13@users.noreply.github.com>
2020-11-19 17:02:25 -0300

    cimabaixo
    c

--- Projeto4/Assets/Scripts/Inimigos/CimaBaixo.cs
+++ Projeto4/Assets/Scripts/Inimigos/CimaBaixo.cs
@@ -5,27 +5,33 @@ using UnityEngine;
 public class CimaBaixo : MonoBehaviour
 {
     
+    
     public float speed;
+    public GameObject reset;
+    public GameObject player;
+    
     bool isRight;
 
-    
     void Update()
     {
-       transform.Translate(Vector2.up * speed * Time.deltaTime);
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
     }
+    
+
+    
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (isRight)
-        {
-            transform.eulerAngles = new Vector3(0, 0, 0);
-            isRight = false;
-        }
-        else
-        {
-            transform.eulerAngles = new Vector3(0, 0, 0);
-            isRight = true;
-            
-        }
+            if (isRight)
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                isRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(180, 0, 0);
+                isRight = true;
+                
+            }
     }
         
     
