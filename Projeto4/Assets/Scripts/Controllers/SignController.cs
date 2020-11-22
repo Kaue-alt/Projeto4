7e38595e1c7829cafa35000a32e737090765bdd9
Kaue-alt <55499541+Kaue-alt@users.noreply.github.com>
2020-11-22 16:00:27 -0300

    k

--- Projeto4/Assets/Scripts/Controllers/SignController.cs
+++ Projeto4/Assets/Scripts/Controllers/SignController.cs
@@ -11,9 +11,9 @@ public class SignController : MonoBehaviour
 
     public GameObject reset;
 
-    private void OnTriggerStay2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        while (other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
             if (this.gameObject.transform.position.y < reset.transform.position.y)
             {
@@ -31,48 +31,65 @@ public class SignController : MonoBehaviour
                 avisoQuiz.SetActive(true);
             }
         }
-        
+       
+
 
     }
 
-    //private void OnTriggerStay2D(Collision2D other)
-    //{
-    //    if (other.CompareTag("Player"))
-    //    {
-    //        if (this.gameObject.transform.position.y < reset.transform.position.y)
-    //        {
-
-    //            avisoCobra.SetActive(true);
-    //        }
-    //        if (this.gameObject.transform.position.x> reset.transform.position.x)
-    //        {
-
-    //            avisoColec.SetActive(true);
-    //        }
-    //        if (this.gameObject.transform.position.y > reset.transform.position.y)
-    //        {
-
-    //            avisoQuiz.SetActive(true);
-    //        }
-    //    }
-    //    else
-    //    {
-    //        avisoCobra.SetActive(false);
-    //        avisoQuiz.SetActive(false);
-    //        avisoColec.SetActive(false);
-
-
-    //    }
-    //}
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerExit2D(Collider2D other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            if (this.gameObject.transform.position.y < reset.transform.position.y)
+            {
+
+                avisoCobra.SetActive(false);
+            }
+            if (this.gameObject.transform.position.x > reset.transform.position.x)
+            {
+
+                avisoColec.SetActive(false);
+            }
+            if (this.gameObject.transform.position.y > reset.transform.position.y)
+            {
+
+                avisoQuiz.SetActive(false);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+
+
+        //private void OnTriggerStay2D(Collision2D other)
+        //{
+        //    if (other.CompareTag("Player"))
+        //    {
+        //        if (this.gameObject.transform.position.y < reset.transform.position.y)
+        //        {
+
+        //            avisoCobra.SetActive(true);
+        //        }
+        //        if (this.gameObject.transform.position.x> reset.transform.position.x)
+        //        {
+
+        //            avisoColec.SetActive(true);
+        //        }
+        //        if (this.gameObject.transform.position.y > reset.transform.position.y)
+        //        {
+
+        //            avisoQuiz.SetActive(true);
+        //        }
+        //    }
+        //    else
+        //    {
+        //        avisoCobra.SetActive(false);
+        //        avisoQuiz.SetActive(false);
+        //        avisoColec.SetActive(false);
+
+
+        //    }
+        //}
+        // Start is called before the first frame update
+
     }
-}
